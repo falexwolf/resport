00d927cf6005d0a63a56704afb8c71f42354b6c3
Alex Wolf <f.alexander.wolf@gmail.com>
2022-02-01 18:07:53 +0100

    fix bug in talks

--- resport/__init__.py
+++ resport/__init__.py
@@ -626,9 +626,10 @@ def main():
     from dirsync import sync
     sync('_assets/', '_site', 'sync')
     shutil.copy('_cv/CV.pdf', '_site/CV.pdf')
-    for directory in ['data', 'talks']:
-        if os.path.exists('_data/'):
-            sync(f'_{directory}/', '_site/{directory}/', 'sync', create=True)
+    if os.path.exists('talks/'):
+        sync('talks/', '_site/talks/', 'sync', create=True)
+    if os.path.exists('_data/'):
+        sync('_data/', '_site/data/', 'sync', create=True)
 
     directories = [
         str(p) for p in Path('.').glob('*')
